08c0a94f8a8a0ddf22962ab9b983f14386192ec2
gustavosv77 <gustavosv.senai77@gmail.com>
2023-11-08 21:50:42 -0300

    feat: initial program

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import BoxWhiteLogin from "../../components/BoxWhiteLogin";
+import "./style.css"
+
+const Login = () => {
+  return (
+    <>
+    <div id="container">
+      <BoxWhiteLogin/>
+    </div>
+    </>
+  );
+};
+
+export default Login;
