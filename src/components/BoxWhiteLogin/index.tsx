51ff84a56e357247310ac22a3a970cd5eb6bab87
gustavosv77 <gustavosv.senai77@gmail.com>
2023-11-09 21:50:19 -0300

    feat: add style partial page login

--- src/components/BoxWhiteLogin/index.tsx
+++ src/components/BoxWhiteLogin/index.tsx
@@ -1,7 +1,14 @@
 import "./style.css";
 
 const BoxWhiteLogin = () => {
-  return <div id="boxWhiteLogin"></div>;
+  return <div id="boxWhiteLogin">
+    <h1 id="title-app">Agendamento de Ambientes Educacionais</h1>
+    <div id="boxWhiteLogin-gradiant">
+      <img id="art" src="public\art.png" alt="mulher com computador" />
+    </div>
+      <img id="art-mobile" src="public\art-mobile.png" alt="mulher com computador" />
+      <img id="logo-senai" src="public\logo-senai.png" alt="logomarca do Senai" />
+  </div>;
 };
 
 export default BoxWhiteLogin;
