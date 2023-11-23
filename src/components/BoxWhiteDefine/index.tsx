1c06b995d302b14809ccda23dce970aab759d678
gustavosv77~ <gustavosv.senai77@gmail.com>
2023-11-23 11:55:40 -0300

    feat: finishing component boxWhiteDefine

--- src/components/BoxWhiteDefine/index.tsx
+++ src/components/BoxWhiteDefine/index.tsx
@@ -11,6 +11,8 @@ const BoxWhiteDefine = () => {
         <h2 id="title-lab1">LABORATÓRIO 3</h2>
         <h2 id="title-lab2">SETEMBRO - 2023</h2>
         <img src="public/calendario-senai.png" alt="calendario simples" id="calendario-img"/>
+        <img src="public/icon-maior.png" alt="icone de maior que" id="maior-icon-def"/>
+        <img src="public/seta-direita-senai.png" alt="seta para a direita" id="seta-direita-def"/>
       </div>  
     </div>
   );
